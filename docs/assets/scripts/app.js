1c758e8ba189278eaffe8e10b3de0ab82ee332a8
yokota4 <yokota@nanomedia.jp>
2025-03-13 10:50:28 +0900

    feat: マップ部分のコーディング。

--- docs/assets/scripts/app.js
+++ docs/assets/scripts/app.js
@@ -1,1 +1,33 @@
-!function(){"use strict";document.addEventListener("DOMContentLoaded",(()=>{}))}();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/scripts/app.js":
+/*!***********************************!*\
+  !*** ./src/assets/scripts/app.js ***!
+  \***********************************/
+/***/ (function() {
+
+eval("\n// import InView from './components/inView';\n// import Accordion from './components/accordion';\n// import Modal from './components/modal';\n\ndocument.addEventListener(\"DOMContentLoaded\", () => {\n\n  // // スクロール 表示領域にて発火\n  // const inViewEl = [...document.querySelectorAll('.js-inView')].map(el => {\n  //   return new InView(el);\n  // });\n\n  // // アコーディオン\n  // const accordionEl = [...document.querySelectorAll('.js-accordion')].map(el => {\n  //   return new Accordion(el, {\n  //     tabs: '.js-accordion-tab',\n  //     panels: '.js-accordion-panel'\n  //   });\n  // });\n\n  // // モーダル\n  // const modalEl = [...document.querySelectorAll('[data-a11y-dialog]')].map(el => {\n  //   return new Modal(el);\n  // });\n\n});\n\n\n//# sourceURL=webpack://static-kit/./src/assets/scripts/app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/scripts/app.js"]();
+/******/ 	
+/******/ })()
+;
--- docs/assets/scripts/viewport.js
+++ docs/assets/scripts/viewport.js
@@ -1,1 +1,118 @@
-!function(){var t={735:function(){(()=>{let t=window.innerWidth;const e=()=>{const t=.01*document.documentElement.clientHeight;document.documentElement.style.setProperty("--vh",`${t}px`)},n=()=>{const t=.01*document.documentElement.clientWidth;document.documentElement.style.setProperty("--vw",`${t}px`)};window.addEventListener("resize",(()=>{n(),t===window.innerWidth&&navigator.userAgent.match(/iPhone|Android.+Mobile/)||(t=window.innerWidth,e())})),e(),n()})()},808:function(){(()=>{const t=document.querySelector('meta[name="viewport"]'),e=()=>{const e=window.outerWidth>390?"width=device-width,initial-scale=1":"width=390";t.getAttribute("content")!==e&&t.setAttribute("content",e)};window.addEventListener("resize",e),e()})()}},e={};function n(o){var i=e[o];if(void 0!==i)return i.exports;var r=e[o]={exports:{}};return t[o](r,r.exports,n),r.exports}n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,{a:e}),e},n.d=function(t,e){for(var o in e)n.o(e,o)&&!n.o(t,o)&&Object.defineProperty(t,o,{enumerable:!0,get:e[o]})},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},function(){"use strict";n(735),n(808)}()}();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/scripts/structure/adjustView.js":
+/*!****************************************************!*\
+  !*** ./src/assets/scripts/structure/adjustView.js ***!
+  \****************************************************/
+/***/ (function() {
+
+eval("/**\n * ウィンドウの横幅と高さをCSS変数として設定する関数。\n * これにより、CSS内でviewportの高さ(vh)や横幅(vw)を変数として利用できるようになります。\n */\n(() => {\n  // 初期のウィンドウの横幅を取得\n  let baseVw = window.innerWidth;\n\n  /**\n   * viewportの高さをCSS変数として設定する関数。\n   */\n  const adjustVh = () => {\n    const vh = document.documentElement.clientHeight * 0.01;\n    document.documentElement.style.setProperty('--vh', `${vh}px`);\n  };\n\n  /**\n   * viewportの横幅をCSS変数として設定する関数。\n   */\n  const adjustVw = () => {\n    const vw = document.documentElement.clientWidth * 0.01;\n    document.documentElement.style.setProperty('--vw', `${vw}px`);\n  };\n\n  // ウィンドウのサイズが変更されたときのイベントリスナー\n  window.addEventListener('resize', () => {\n    adjustVw();\n    if (baseVw === window.innerWidth && navigator.userAgent.match(/iPhone|Android.+Mobile/)) {\n      // 画面の横幅に変動がない場合は、高さの再計算をスキップ\n      return;\n    }\n    // 画面の横幅に変動があった場合のみ、高さを再計算\n    baseVw = window.innerWidth;\n    adjustVh();\n  });\n\n  // 初期化時に横幅と高さのCSS変数を設定\n  adjustVh();\n  adjustVw();\n})();\n\n// Usage Examples\n// -----------------\n// // CSS\n// .eml {\n//   width: calc(var(--vw) * 100); // 100vw\n//   height: calc(var(--vh) * 100); // 100vh\n//   width: calc(var(--vw) * 50); // 50vw\n//   height: calc(var(--vh) * 80); // 80vh\n// }\n\n\n\n//# sourceURL=webpack://static-kit/./src/assets/scripts/structure/adjustView.js?");
+
+/***/ }),
+
+/***/ "./src/assets/scripts/structure/adjustViewport.js":
+/*!********************************************************!*\
+  !*** ./src/assets/scripts/structure/adjustViewport.js ***!
+  \********************************************************/
+/***/ (function() {
+
+eval("/**\n * 390px以下のデバイス向けにviewportを調整する関数。\n * 390pxより大きいデバイスでは、通常のviewport設定を使用。\n * 390px以下のデバイスでは、viewportの幅を390pxに固定。\n */\n(() => {\n  // viewportのmetaタグを取得\n  const viewport = document.querySelector('meta[name=\"viewport\"]');\n\n  /**\n   * viewportの設定を調整する関数。\n   */\n  const adjustViewport = () => {\n    // ウィンドウの外部幅が390pxより大きい場合と390px以下の場合で、viewportの設定を変更\n    const value =\n      window.outerWidth > 390\n        ? 'width=device-width,initial-scale=1'\n        : 'width=390';\n\n    // 現在のviewportの設定と新しい設定が異なる場合のみ、viewportを更新\n    if (viewport.getAttribute('content') !== value) {\n      viewport.setAttribute('content', value);\n    }\n  };\n\n  // ウィンドウのサイズが変更されたときのイベントリスナーを追加\n  window.addEventListener('resize', adjustViewport);\n\n  // 初期化時にviewportを調整\n  adjustViewport();\n})();\n\n\n//# sourceURL=webpack://static-kit/./src/assets/scripts/structure/adjustViewport.js?");
+
+/***/ }),
+
+/***/ "./src/assets/scripts/viewport.js":
+/*!****************************************!*\
+  !*** ./src/assets/scripts/viewport.js ***!
+  \****************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _structure_adjustView__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./structure/adjustView */ \"./src/assets/scripts/structure/adjustView.js\");\n/* harmony import */ var _structure_adjustView__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_structure_adjustView__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _structure_adjustViewport__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./structure/adjustViewport */ \"./src/assets/scripts/structure/adjustViewport.js\");\n/* harmony import */ var _structure_adjustViewport__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_structure_adjustViewport__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\n\n\n//# sourceURL=webpack://static-kit/./src/assets/scripts/viewport.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/assets/scripts/viewport.js");
+/******/ 	
+/******/ })()
+;
